b6b5cb944b2d723d0a588e07d6b97c2f1345dd63
disit <info@disit.org>
2020-01-31 23:15:46 +0100

    fix

--- DataCity-Small/setup.sh
+++ DataCity-Small/setup.sh
@@ -1,12 +1,17 @@
 chmod -R a+w iotapp-nr1
 chmod -R a+w iotapp-nr2
 chmod a+w iot-directory-certificate
+mkdir -p iot-directory-log
 chmod a+w iot-directory-log
+mkdir servicemap-conf/logs
+mkdir servicemap-iot-conf/logs
+mkdir servicemap-iot-conf/logs/insert
+mkdir servicemap-iot-conf/logs/delete
 chmod a+w servicemap-conf/logs
 chmod a+w servicemap-iot-conf/logs
 chmod a+w servicemap-iot-conf/logs/insert
 chmod a+w servicemap-iot-conf/logs/delete
 chmod a+w nifi/conf
-chmod a+x nifi/conf/flow.xml.gz
+chmod a+w nifi/conf/flow.xml.gz
 chmod a+w nifi/extensions
 
